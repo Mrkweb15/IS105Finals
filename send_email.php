43eb23b9738703d32648ce6226a071f45b66d593
Mrkweb15 <166189464+Mrkweb15@users.noreply.github.com>
2024-05-26 14:04:02 +0800

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = 'kalabasafloat@gmail.com';
+    
+    $subject = 'New Contact Form Submission';
+    
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+    
+    // Build the email headers.
+    $headers = "From: $name <$email>";
+    
+    if (mail($to, $subject, $email_content, $headers)) {
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
